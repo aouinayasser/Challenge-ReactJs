375b34e996b8c2869aab37c64278a254eb43fc3c
aouinayasser <90246671+aouinayasser@users.noreply.github.com>
2021-10-10 18:26:38 +0200

    fc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <body>
+    <header className="header">
+      <h1 className="h">John Doe</h1>
+      <ul className="navmenu">
+        <li><a href="#" className="a">About</a></li>
+        <li><a href="#" className="a">Projects</a></li>
+        <li><a href="#" className="a">Contact</a></li>
+      </ul>
+    </header>
+    <section id="about-me">
+      <h1 className="h">
+        Hello, my name is
+        <span className="rotate text-important">John doe</span>,<br />
+        and i make horrible websites.
+      </h1>
+      <img className="avatar" src="./images/John-Doe.jpg" alt="jhon-doe" />
+    </section>
+    <section id="projects">
+      <h2 className="text-important h">Projects</h2>
+      <div className="projects-container">
+        
+        <div className="project-card">
+          <img src="./images/proj1.jpg" alt="project" />
+          <h3 className="h">Project 1</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus ex natus quis beatae perspiciatis repellat laudantium
+            aliquam.
+          </p>
+          <p><a href="#">Github Link</a></p>
+        </div>
+       
+        <div className="project-card">
+          <img src="./images/proj2.jpg" alt="project" />
+          <h3 className="h">Project 2</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus ex natus quis beatae perspiciatis repellat laudantium
+            aliquam.
+          </p>
+          <p><a href="#">Github Link</a></p>
+        </div>
+        
+        <div className="project-card">
+          <img src="./images/proj3.jpg" alt="project" />
+          <h3 className="h">Project 3</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus ex natus quis beatae perspiciatis repellat laudantium
+            aliquam.
+          </p>
+          <p><a href="#">Github Link</a></p>
+        </div>
+        
+        <div className="project-card">
+          <img src="./images/proj4.jpg" alt="project" />
+          <h3 className="h">Project 4</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus ex natus quis beatae perspiciatis repellat laudantium
+            aliquam.
+          </p>
+          <p><a href="#">Github Link</a></p>
+        </div>
+      </div>
+    </section>
+    <section id="contact">
+      <h2 className="text-important h">contact me</h2>
+      <form>
+        <label>Full Name</label>
+        <input type="text" placeholder="Email" />
+        <label>Email</label>
+        <input type="email" placeholder="Email" />
+        <label>Message</label>
+        <textarea rows="5" cols="40"></textarea>
+        <button className="button">Send</button>
+      </form>
+    </section>
+    <footer className="footer">
+      <p>Copyritghts &copy; GOMYCODE 2020</p>
+    </footer>
+  </body>
     </div>
   );
 }
